cb1dbb6565bca8b8f6049b0214ca93fe5be037b4
MaksymBuriachenko <ipadtest3347@gmail.com>
2025-03-15 00:15:18 +0200

    Add files via upload
    FIRST LESSON JS COURS

---
+++ src/index.js
@@ -0,0 +1,20 @@
+console.log('ппривіт усім учням і тобі також');
+
+const number  = 123;
+var str1 = 'test string';
+let bool = false;
+let integer  = 314;
+
+console.log({
+    number,
+    str1,
+    bool,
+    integer
+
+})
+
+console.log ({
+numberType: typeof  number,
+str1Type: typeof str1
+
+})
